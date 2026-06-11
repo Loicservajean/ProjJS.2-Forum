62c4d99b66485a38f03db2c19ffd8810a0b2b99b
ghuart9207-cmd <ghuart9207@gmail.com>
2026-06-11 10:46:35 +0200

    Ajout Serveur Static

--- routers/web.router.go
+++ routers/web.router.go
@@ -1,11 +1,16 @@
 package routers
 
 import (
+	"net/http"
 	"rompelago/controllers"
 
 	"github.com/gorilla/mux"
 )
 
 func RegisterWebRoutes(r *mux.Router, tc *controllers.WebFilsControllers) {
-	r.HandleFunc("/", nil)
+	r.PathPrefix("/static/").Handler(
+		http.StripPrefix("/static/", http.FileServer(http.Dir("static"))),
+	)
+
+	r.HandleFunc("/forum", tc.ListPage).Methods("GET")
 }
