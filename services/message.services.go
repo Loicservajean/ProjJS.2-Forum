19c936aef3e1c8c5700d79ab0c648060e0bce4ab
ghuart9207-cmd <ghuart9207@gmail.com>
2026-06-18 13:22:09 +0200

    Ajout tri Partie Finale

--- services/message.services.go
+++ services/message.services.go
@@ -15,11 +15,11 @@ func InitPostDiscussionService(repo *repositories.PostRepositories) *PostDiscuss
 	return &PostDiscussionService{PostRepository: repo}
 }
 
-func (s *PostDiscussionService) ReadByFilId(filId int, limit, offset int) ([]models.PostModel, error) {
+func (s *PostDiscussionService) ReadByFilId(filId int, limit, offset int, sort string) ([]models.PostModel, error) {
 	if filId <= 0 {
 		return nil, fmt.Errorf("identifiant de fil invalide : %d", filId)
 	}
-	return s.PostRepository.ReadPostsByFilId(filId, limit, offset)
+	return s.PostRepository.ReadPostsByFilId(filId, limit, offset, sort)
 }
 
 func (s *PostDiscussionService) CountByFilId(filId int) (int, error) {
